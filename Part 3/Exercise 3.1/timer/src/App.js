2d7aec1f8352d81e2bb06df6a8d85ef0f6cf14bc
Artoiss <aarttu@windowslive.com>
2021-06-23 18:56:33 +0300

    Initial commit for application.

---
+++ Part 3/Exercise 3.1/timer/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Start timer by pressing the button below.
+        </p>
+        <Button> asd </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Part 3/Exercise 3.1/timer/src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+const Button=() => {
+
+  return (
+    <div>
+      <button className="button_style" type="button">
+      GO
+      </button>
+    </div>
+  );
+
+}
+
+export default Button;

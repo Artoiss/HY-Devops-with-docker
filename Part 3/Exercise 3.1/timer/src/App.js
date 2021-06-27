9cb5f6d84fb25e739c7db58858fb0cac9b58c9ba
Artoiss <aarttu@windowslive.com>
2021-06-27 16:37:01 +0300

    Simple stopwatch ready for contanization.

--- Part 3/Exercise 3.1/timer/src/App.js
+++ Part 3/Exercise 3.1/timer/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Button from './components/Button.js';
 
+// Reference to timer logic https://dev.to/abdulbasit313/how-to-develop-a-stopwatch-in-react-js-with-custom-hook-561b
 function App() {
-  const[selected, setSelected] = useState(0)
+  const[timer, setTimer] = useState(0)
+  const[active, setActive] = useState(0)
   const handle = (props) => {
-    setSelected(props + 1)
+    setActive(timeFormats())
+    clearInterval(countRef.current)
+    setTimer(0)
+    countRef.current = setInterval(() => {
+    setTimer((timer) => timer + 1)
+    }, 1000)
+  }
+  const countRef = useRef(null)
+
+  const timeFormats = () => {
+
+    const seconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    return `${minutes} : ${seconds}`
   }
 
   return (
@@ -14,8 +29,15 @@ function App() {
         <p>
           Start timer by pressing the button below.
         </p>
-        <Button onClick={() => handle(selected)} title={"GO"}></Button>
-        {selected}
+        <Button onClick={() => handle(timer)} title={"Start/Stop"}></Button>
+        <p>
+        Time elapsed:
+        </p>
+        {timeFormats()}
+        <p>
+        Last time:
+        </p>
+        {active}
       </header>
     </div>
   );
